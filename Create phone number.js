b4f40cf05363b319b17582f227ec1ee4385365e6
SalazarFco <149215896+SalazarFco@users.noreply.github.com>
2023-10-28 01:55:26 -0300

    Update Create phone number.js

--- Create phone number.js
+++ Create phone number.js
@@ -1,3 +1,5 @@
+// https://www.codewars.com/kata/525f50e3b73515a6db000b83
+
 function createPhoneNumber(numbers){
     const greeting = `(${numbers[0]}${numbers[1]}${numbers[2]}) ${numbers[3]}${numbers[4]}${numbers[5]}-${numbers[6]}${numbers[7]}${numbers[8]}${numbers[9]}`;
     return greeting;
@@ -5,5 +7,5 @@ function createPhoneNumber(numbers){
 
 
 
-console.log(createPhoneNumber([1, 2, 3, 4, 5, 6, 7, 8, 9, 0]))
+
 
